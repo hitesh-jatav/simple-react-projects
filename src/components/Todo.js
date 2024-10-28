3c6bd38c955dff09e98ca6057368ee9f9ee8a40e
hitesh-jatav <hkjatav@gmail.com>
2024-10-28 22:31:05 +0530

    todo design

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const TodoList = () => {
     const [tasks, setTasks] = useState([]);
     const [newTask, setNewTask] = useState('');
 
+    useEffect(() => {
+        const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+        setTasks(storedTasks);
+    }, []);
+
+
     const addTask = () => {
         if (newTask.trim()) {
             setTasks([...tasks, { text: newTask, completed: false }]);
             setNewTask('');
+            localStorage.setItem('tasks', JSON.stringify([...tasks, { text: newTask, completed: false }]));
         }
     };
 
@@ -21,32 +28,37 @@ const TodoList = () => {
     const deleteTask = (index) => {
         const updatedTasks = tasks.filter((_, i) => i !== index);
         setTasks(updatedTasks);
+        localStorage.setItem('tasks', JSON.stringify(tasks.filter((_, i) => i !== index)));
+
     };
 
     return (
-        <div className='todo-wrapper'>
-            <div className="todo-list">
-                <h1>Todo List</h1>
-                <div className="input-container">
-                    <input
-                        type="text"
-                        value={newTask}
-                        onChange={(e) => setNewTask(e.target.value)}
-                        placeholder="Add a new task"
-                    />
-                    <button onClick={addTask}>Add</button>
+        <div className="todo-page">
+            <h1>Todo List</h1>
+            <div className="search">
+                <input
+                    type="text"
+                    value={newTask}
+                    onChange={(e) => setNewTask(e.target.value)}
+                    placeholder='Add a task!'
+                />
+                <div className="button-src">
+                    <button onClick={addTask}>
+                        <i className="ri-search-2-line"></i>
+                    </button>
                 </div>
-                <ul>
-                    {tasks.map((task, index) => (
-                        <li key={index} className={task.completed ? 'completed' : ''}>
-                            <span onClick={() => toggleTaskCompletion(index)}>
-                                {task.text}
-                            </span>
-                            <button onClick={() => deleteTask(index)}>Delete</button>
-                        </li>
-                    ))}
-                </ul>
             </div>
+
+            {tasks.map((task, index) => (
+                <p key={index} className={task.completed ? 'completed ' : '' + 'd-flex justify-content-between w-100 my-2 px-4 py-2'}>
+                    <span onClick={() => toggleTaskCompletion(index)}>
+                        {task.text}
+                    </span>
+                    <button className='btn' onClick={() => deleteTask(index)}>
+                        <i className="ri-delete-bin-line"></i>
+                    </button>
+                </p>
+            ))}
         </div>
     );
 };
